11ddbc4733c5747dc110513495928c3d044c5ace
Mara <alaneraerts@live.be>
2021-03-21 15:47:30 +0100

    cards ui

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,37 +1,38 @@
 <template>
-  <v-card
-    class="mx-auto"
-    max-width="344"
-    outlined
-  >
-    <v-list-item three-line  v-for="product in products">
-      <v-list-item-content>
-        <div class="overline mb-4">
-          OVERLINE
-        </div>
-        <v-list-item-title class="headline mb-1">
-          {{ product.title }}
-        </v-list-item-title>
-        <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
-      </v-list-item-content>
+  <v-container fluid>
+    <v-row>
+      <v-col v-for="product in products">
+        <v-card
+          class="mx-auto"
+          max-width="600"
+          outlined
+        >
+        <v-list-item three-line  >
+          <v-list-item-content>
+            <div class="overline mb-4">
+              {{ product.price }} $
+            </div>
+            <v-list-item-title class="headline mb-1">
+              {{ product.title }}
+            </v-list-item-title>
+            <v-list-item-subtitle>{{ product.description }}</v-list-item-subtitle>
+          </v-list-item-content>
 
-      <v-list-item-avatar
-        tile
-        size="80"
-        color="grey"
-      ></v-list-item-avatar>
-    </v-list-item>
+        </v-list-item>
 
-    <v-card-actions>
-      <v-btn
-        outlined
-        rounded
-        text
-      >
-        Button
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+        <v-card-actions>
+          <v-btn
+            outlined
+            rounded
+            text
+          >
+            Edit
+          </v-btn>
+        </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 
